78ac9e215df927b348bd340fb681183de3e7097d
MuhammadArif686 <arief86.ma@gmail.com>
2018-10-18 10:12:04 +0700

    versi 2.19.0

--- Basic_CRUD/Form1.Designer.vb
+++ Basic_CRUD/Form1.Designer.vb
@@ -22,9 +22,256 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnKeluar = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnBatal = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.cmbKelamin = New System.Windows.Forms.ComboBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtAlamat = New System.Windows.Forms.TextBox()
+        Me.txtNoTelepon = New System.Windows.Forms.TextBox()
+        Me.txtKode = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnKeluar)
+        Me.GroupBox1.Controls.Add(Me.btnHapus)
+        Me.GroupBox1.Controls.Add(Me.btnBatal)
+        Me.GroupBox1.Controls.Add(Me.btnEdit)
+        Me.GroupBox1.Controls.Add(Me.btnSimpan)
+        Me.GroupBox1.Controls.Add(Me.btnTambah)
+        Me.GroupBox1.Controls.Add(Me.cmbKelamin)
+        Me.GroupBox1.Controls.Add(Me.txtNama)
+        Me.GroupBox1.Controls.Add(Me.txtEmail)
+        Me.GroupBox1.Controls.Add(Me.txtAlamat)
+        Me.GroupBox1.Controls.Add(Me.txtNoTelepon)
+        Me.GroupBox1.Controls.Add(Me.txtKode)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(579, 246)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "[ Form Kontak ]"
+        '
+        'btnKeluar
+        '
+        Me.btnKeluar.Location = New System.Drawing.Point(468, 208)
+        Me.btnKeluar.Name = "btnKeluar"
+        Me.btnKeluar.Size = New System.Drawing.Size(75, 23)
+        Me.btnKeluar.TabIndex = 4
+        Me.btnKeluar.Text = "Keluar"
+        Me.btnKeluar.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(387, 208)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 4
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnBatal
+        '
+        Me.btnBatal.Location = New System.Drawing.Point(299, 208)
+        Me.btnBatal.Name = "btnBatal"
+        Me.btnBatal.Size = New System.Drawing.Size(75, 23)
+        Me.btnBatal.TabIndex = 4
+        Me.btnBatal.Text = "Batal"
+        Me.btnBatal.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(216, 208)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEdit.TabIndex = 4
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(135, 208)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 4
+        Me.btnSimpan.Text = "SImpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(52, 208)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.TabIndex = 4
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'cmbKelamin
+        '
+        Me.cmbKelamin.FormattingEnabled = True
+        Me.cmbKelamin.Items.AddRange(New Object() {"L", "P"})
+        Me.cmbKelamin.Location = New System.Drawing.Point(110, 136)
+        Me.cmbKelamin.Name = "cmbKelamin"
+        Me.cmbKelamin.Size = New System.Drawing.Size(51, 21)
+        Me.cmbKelamin.TabIndex = 3
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(110, 81)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(211, 20)
+        Me.txtNama.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(387, 81)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(186, 20)
+        Me.txtEmail.TabIndex = 2
+        '
+        'txtAlamat
+        '
+        Me.txtAlamat.Location = New System.Drawing.Point(387, 136)
+        Me.txtAlamat.Multiline = True
+        Me.txtAlamat.Name = "txtAlamat"
+        Me.txtAlamat.Size = New System.Drawing.Size(186, 54)
+        Me.txtAlamat.TabIndex = 2
+        '
+        'txtNoTelepon
+        '
+        Me.txtNoTelepon.Location = New System.Drawing.Point(387, 36)
+        Me.txtNoTelepon.Name = "txtNoTelepon"
+        Me.txtNoTelepon.Size = New System.Drawing.Size(186, 20)
+        Me.txtNoTelepon.TabIndex = 2
+        '
+        'txtKode
+        '
+        Me.txtKode.Location = New System.Drawing.Point(110, 36)
+        Me.txtKode.Name = "txtKode"
+        Me.txtKode.Size = New System.Drawing.Size(100, 20)
+        Me.txtKode.TabIndex = 2
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(336, 139)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Alamat :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(336, 84)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Email :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(336, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "No Tlp :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Jenis Kelamin :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Nama :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Kode :"
+        '
+        'DGV
+        '
+        Me.DGV.AllowUserToAddRows = False
+        Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Location = New System.Drawing.Point(13, 265)
+        Me.DGV.Name = "DGV"
+        Me.DGV.ReadOnly = True
+        Me.DGV.Size = New System.Drawing.Size(579, 190)
+        Me.DGV.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(604, 467)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form Kontak"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnKeluar As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnBatal As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents cmbKelamin As ComboBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtAlamat As TextBox
+    Friend WithEvents txtNoTelepon As TextBox
+    Friend WithEvents txtKode As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DGV As DataGridView
 End Class
--- Basic_CRUD/Form1.vb
+++ Basic_CRUD/Form1.vb
@@ -1,3 +1,131 @@
 ﻿Public Class Form1
 
+    Sub KosongkanForm()
+        txtKode.Text = ""
+        txtNama.Text = ""
+        txtNoTelepon.Text = ""
+        txtEmail.Text = ""
+        txtAlamat.Text = ""
+        txtKode.Focus()
+    End Sub
+
+    Sub MatikanForm()
+        txtKode.Enabled = False
+        txtNama.Enabled = False
+        cmbKelamin.Enabled = False
+        txtNoTelepon.Enabled = False
+        txtEmail.Enabled = False
+        txtAlamat.Enabled = False
+    End Sub
+
+    Sub HidupkanForm()
+        txtKode.Enabled = True
+        txtNama.Enabled = True
+        cmbKelamin.Enabled = True
+        txtNoTelepon.Enabled = True
+        txtEmail.Enabled = True
+        txtAlamat.Enabled = True
+    End Sub
+
+    Sub TampilkanData()
+        Call KoneksiDB()
+        DA = New OleDb.OleDbDataAdapter("select * from kontak", CONN)
+        DS = New DataSet
+        DA.Fill(DS)
+        DGV.DataSource = DS.Tables(0)
+        DGV.ReadOnly = True
+    End Sub
+
+    Private Sub DGV_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DGV.CellMouseClick
+        On Error Resume Next
+        txtKode.Text = DGV.Rows(e.RowIndex).Cells(0).Value
+        txtNama.Text = DGV.Rows(e.RowIndex).Cells(1).Value
+        cmbKelamin.Text = DGV.Rows(e.RowIndex).Cells(2).Value
+        txtNoTelepon.Text = DGV.Rows(e.RowIndex).Cells(3).Value
+        txtEmail.Text = DGV.Rows(e.RowIndex).Cells(4).Value
+        txtAlamat.Text = DGV.Rows(e.RowIndex).Cells(5).Value
+
+        Call HidupkanForm()
+        txtKode.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+
+    Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Call HidupkanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btnBatal_Click(sender As Object, e As EventArgs) Handles btnBatal.Click
+        Call MatikanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        If txtKode.Text = "" Or txtNama.Text = "" Or txtEmail.Text = "" Then
+            MsgBox("Data Tidak Lengkap, Silahkan Dilengkapi Terlebih Dahulu")
+            Exit Sub
+        Else
+            Call KoneksiDB()
+            CMD = New OleDb.OleDbCommand("select * from kontak where kode = '" & txtKode.Text & "'", CONN)
+            DR = CMD.ExecuteReader()
+            DR.Read()
+            If Not DR.HasRows Then
+                Call KoneksiDB()
+                Dim simpan As String
+                simpan = "insert into kontak values ('" & txtKode.Text & "', '" & txtNama.Text & "', '" & cmbKelamin.Text & "', '" & txtNoTelepon.Text & "', '" & txtEmail.Text & "', '" & txtAlamat.Text & "')"
+                CMD = New OleDb.OleDbCommand(simpan, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Data Berhasil Diinputkan")
+            Else
+                MsgBox("Kode yang anda masukkan sudah tersedia, silahkan masukkan dengan kode lainnya")
+            End If
+
+            Call MatikanForm()
+            Call KosongkanForm()
+            Call TampilkanData()
+
+        End If
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If txtKode.Text = "" Or txtNama.Text = "" Or txtNoTelepon.Text = "" Then
+            MsgBox("Data belum lengkap, silahkan dilengkapi terlebih dahulu")
+            Exit Sub
+        Else
+            Call KoneksiDB()
+            CMD = New OleDb.OleDbCommand("update kontak set nama_teman = '" & txtNama.Text & "', jenis_kelamin = '" & cmbKelamin.Text & "', no_telp = '" & txtNoTelepon.Text & "', email = '" & txtEmail.Text & "', alamat_rumah = '" & txtAlamat.Text & "' where kode = '" & txtKode.Text & "'", CONN)
+            CMD.ExecuteNonQuery()
+            MsgBox("Data Berhasil Diperbaharui")
+
+        End If
+
+        Call MatikanForm()
+        Call KosongkanForm()
+        Call TampilkanData()
+
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If txtKode.Text = "" Then
+            MsgBox("Tidak ada data yang dihapus")
+            Exit Sub
+        Else
+            Call KoneksiDB()
+            CMD = New OleDb.OleDbCommand("delete from kontak where kode = '" & txtKode.Text & "'", CONN)
+            CMD.ExecuteNonQuery()
+            MsgBox("Data Berhasil Dihapus")
+            Call MatikanForm()
+            Call KosongkanForm()
+            Call TampilkanData()
+
+        End If
+    End Sub
 End Class
